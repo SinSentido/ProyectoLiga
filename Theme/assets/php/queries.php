ba6ea64584ce25adf55abcab502e5bce8da110b6
Jose Luis <jldrm1996@gmail.com>
2020-02-26 20:52:36 +0100

    Algunas pruebas realizada
    He preparado un poco algunas pruebas pero no he podido ejecutarlas porque no tengo la BD pero puedes echarle un  vistazo haber que te parece -> estan realizadas en el fichero queries.php

--- Theme/assets/php/queries.php
+++ Theme/assets/php/queries.php
@@ -1,2 +1,37 @@
 <!-- Comprueba si se establece asi el meedo Jose que yo creo que si-->
+<?php 
+/**********************PRUEBAS:*/
 require '../Medoo/Medoo.php';
+
+//Importamos la clase de Medoo
+use Medoo\Medoo;
+
+// Con el Medoo creamos el objeto de la DB
+$database = new Medoo([
+    // required
+	'database_type' => 'mysql',
+	'database_name' => 'basketLeague',
+	'server' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+    ]);
+
+getAllTeams($database);
+
+
+
+
+
+
+
+/**********************FUNCIONES:*/
+// Obtenemos todos los campos de la tabla equipo 
+function getAllTeams($baseData){
+    $datas = $baseData->select("equipo",["*"] );
+    // Mostramos todos los datos
+    foreach($datas as $data){
+	    echo "codEquipo:" . $data["codEquipo"] . " - nombreEquipo:" . $data["nombreEquipo"] . "<br/>";
+    }
+}
+
+?>
